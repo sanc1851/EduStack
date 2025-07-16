87f99992b16d58cae52a5d837543ef53e6462614
Sanchita Singh <youremail@example.com>
2025-07-16 17:27:55 +0530

    first commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      height: {
+        'heightWithoutNavbar': "calc(100vh - 80px)",
+      },
+      backgroundImage: {
+        'unsplashBgImage': "url('/img/manit.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
+
+
